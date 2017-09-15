ee657b4b96de3f59311f2099bab2a9d055c34c5e
KonstantinVlasov <konstantinnvlasov@gmail.com>
2017-09-15 13:31:58 +0300

    update appMenu with submenus

--- src/components/appMenu.vue
+++ src/components/appMenu.vue
@@ -1,33 +1,94 @@
 <template lang="pug">
   .app-menu
-    a.app-menu-logo(href="https://icons8.com" v-html="icons.logo")
-    a.app-menu-item.is-logo(href="https://icons8.com") Icons8
+    a.logo(href="https://icons8.com" v-html="icons.logo")
+    a.item.is-logo(href="https://icons8.com") Icons8
 
-    .app-menu-center
-      nuxt-link.app-menu-item.is-active(to="/icon/pack/free-icons/all") Icons
-      a.app-menu-item(href="https://icons8.com") Download
-      a.app-menu-item(href="https://icons8.com/request-icon") Request
-      a.app-menu-item(href="https://icons8.com/paid-license-99") Buy
-      a.app-menu-item(href="https://icons8.com") Labs
-      a.app-menu-item(href="https://icons8.com/blog") Blog
+    .center
+      nuxt-link.item.is-active(to="/icon/pack/free-icons/all") Icons
+      .item
+        app-popup.submenu(position="bottom-center") Download
+          div(slot="content")
+            .ribbon All Free
+            .column
+              a.submenu-item(href="https://icons8.com/app")
+                img.image(src="../assets/images/icons8-96.png")
+                | Icons8 App
+                br
+                small All our icons offline on your computer
+              a.submenu-item(href="https://icons8.com/lunacy")
+                img.image(src="../assets/images/lunacy-96.png")
+                | Icons8 Lunacy
+                br
+                small Sketch Viewer for Windows
+
+      a.item(href="https://icons8.com/request-icon") Request
+      a.item(href="https://icons8.com/paid-license-99") Buy
+      .item
+        app-popup.submenu.is-wide(position="bottom-center") Labs
+          div(slot="content")
+            .ribbon All Free
+            .column.is-half
+              a.submenu-item(href="https://img.icons8.com/")
+                img.image(src="../assets/images/stoned-bat-96.png")
+                | omg-img
+                br
+                small Search icons by writing URL
+              a.submenu-item(href="https://icons8.com/imessage")
+                img.image(src="../assets/images/mongrol-96.png")
+                | iMessage Stickers
+                br
+                small 3,900 flat color icons in your iMessages
+              a.submenu-item(href="https://icons8.com/line-awesome")
+                img.image(src="../assets/images/scout-knot-96.png")
+                | Line Awesome
+                br
+                small Replace FontAwesome with modern line icons
+              a.submenu-item(href="https://icons8.com/pedro")
+                img.image(src="../assets/images/comet-96.png")
+                | Cosmic Pedro
+                br
+                small Choose icon, add text, post to social media
+
+            .column.is-half
+              a.submenu-item(href="http://ai.icons8.com/Scribble/Search")
+                img.image(src="../assets/images/autograph-96.png")
+                | Scribble AI
+                br
+                small Search icons by drawing
+              a.submenu-item(href="https://www.iconpharm.com/")
+                img.image(src="../assets/images/sheep-96.png")
+                | IconPharm
+                br
+                small Icons8 technology for open source icons
+              a.submenu-item(href="https://icons8.com/welovesvg")
+                img.image(src="../assets/images/year-of-monkey-96.png")
+                | We Love SVG
+                br
+                small Insert open source icons into your web apps
+              a.submenu-item(href="https://github.com/icons8/impresser")
+                img.image(src="../assets/images/robot-96.png")
+                | Impresser
+                br
+                small As a front-end developer, I want HTML for bots
+      a.item(href="https://icons8.com/blog") Blog
 
 
-    .app-menu-profile(v-if="!user.isGuest")
-      a.app-menu-item(href="https://icons8.com/profile/summary") My Account
-      .app-menu-item(
+    .profile(v-if="!user.isGuest")
+      a.item(href="https://icons8.com/profile/summary") My Account
+      .item(
         v-html="icons.notifications"
       )
-      .app-menu-item(
+      .item(
         v-on:click="logoutUser"
         v-html="icons.logout"
       )
 
-    .app-menu-profile(v-if="user.isGuest")
-      .app-menu-item(v-on:click="login") Login
-      .app-menu-item(v-on:click="register") Register
+    .profile(v-if="user.isGuest")
+      .item(v-on:click="login") Login
+      .item(v-on:click="register") Register
 
-    .app-menu-language
-      .app-menu-item(
+    .language
+      .item(
         v-on:click=""
         v-html="icons.locale.en"
       )
@@ -80,7 +141,7 @@
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   @import '../assets/css/variables';
   @import '../assets/css/breakpoints';
 
@@ -97,7 +158,7 @@
       padding: 0 1rem;
     }
 
-    .app-menu-logo {
+    .logo {
       display: inline-block;
       width: $menu-height;
       height: $menu-height;
@@ -107,12 +168,12 @@
         margin: 0 auto;
       }
 
-      &>svg {
+      & /deep/ > svg {
         fill: white;
       }
     }
 
-    .app-menu-item {
+    .item {
       display: inline-block;
       padding: 0 0.75rem;
       vertical-align: top;
@@ -136,7 +197,48 @@
       }
     }
 
-    .app-menu-center {
+    .submenu {
+      position: relative;
+      & /deep/ .app-popup-content {
+        position: fixed;
+        width: 400px;
+        padding: 2rem 2rem 1rem;
+        text-align: left;
+        color: black;
+        line-height: 1.4;
+        cursor: default;
+      }
+      &.is-wide /deep/ .app-popup-content {
+        width: 800px;
+      }
+    }
+
+    .column {
+      &.is-half {
+        width: 50%;
+        float: left;
+      }
+    }
+
+    .submenu-item {
+      display: block;
+      margin-bottom: 1rem;
+      border-color: transparent;
+      text-decoration: none;
+      cursor: pointer;
+
+      &>small {
+        color: $color-font-light;
+      }
+    }
+
+    .image {
+      width: 48px;
+      float: left;
+      margin-right: 1rem;
+    }
+
+    .center {
       position: absolute;
       top: 0;
       left: 50%;
@@ -159,16 +261,16 @@
       }
     }
 
-    .app-menu-profile {
+    .profile {
       float: right;
 
       @media (max-width: $responsive-menu-short) {
         display: none;
       }
 
-      .app-menu-item {
+      .item {
         padding: 0 0.5rem;
-        &>svg {
+        & /deep/ > svg {
           fill: white;
           width: 1.25rem;
           vertical-align: sub;
@@ -176,15 +278,15 @@
       }
     }
 
-    .app-menu-language {
+    .language {
       float: right;
 
       @media (max-width: $responsive-menu-short) {
         display: none;
       }
-      .app-menu-item {
+      .item {
         padding: 0.375rem 0.5rem 0;
-        &>svg {
+        & /deep/ > svg {
           width: 30px;
           vertical-align: sub;
         }
