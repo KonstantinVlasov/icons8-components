dc51aacd246667a2249c3a1d45f4eff9527d7c50
KonstantinVlasov <konstantinnvlasov@gmail.com>
2017-09-18 02:05:36 +0300

    Bump to 0.1.2

--- docs/_nuxt/pages/css.6bbd671db319de4cdedb.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([5],{"479T":function(t,e,n){"use strict";e.a={name:"CSSDocs",data:function(){return{}}}},"7/xD":function(t,e,n){"use strict";var s=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"docs"},[n("h2",[t._v("CSS Components")]),n("button-docs")],1)},a=[],c={render:s,staticRenderFns:a};e.a=c},mUtx:function(t,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var s=n("479T"),a=n("7/xD"),c=n("VU/8"),r=c(s.a,a.a,null,null,null);e.default=r.exports}});
-//# sourceMappingURL=css.6bbd671db319de4cdedb.js.map
